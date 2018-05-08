09feb09cfbeb3da66e980ea635703d74fdc6e667
jscstc007 <364080573@qq.com>
2018-05-08 18:14:55 +0800

    调整资源加载模块;

--- EasyUIFramework/Assets/EasyUIFrameWork/Scripts/Resource/ResourceManager.cs
+++ EasyUIFramework/Assets/EasyUIFrameWork/Scripts/Resource/ResourceManager.cs
@@ -8,6 +8,9 @@ using System;
 /// </summary>
 public class ResourceManager : MonoBehaviour {
 
+    /// <summary> 异步加载资源的回调 </summary>
+    public delegate void OnLoadAsset<T>(T asset);
+
     #region 单例模式
     private static ResourceManager instance;
     public static ResourceManager Instance
@@ -60,7 +63,7 @@ public class ResourceManager : MonoBehaviour {
         {
             resource = Resources.Load<T>(path);
             //添加进缓存
-            LoadedResource.Add(path, resource as GameObject);   
+            LoadedResource.Add(path, resource as UnityEngine.Object);   
         }
         return resource;
     }
@@ -91,28 +94,33 @@ public class ResourceManager : MonoBehaviour {
     private Dictionary<string, AssetBundle> LoadedAssetbundles = new Dictionary<string, AssetBundle>();
 
     /// <summary>
+    /// 当前正在加载的ab资源
+    /// </summary>
+    private Dictionary<string, OnLoadAsset<UnityEngine.Object>> LoadingAssetbundles = new Dictionary<string, OnLoadAsset<UnityEngine.Object>>();
+
+
+    /// <summary>
     /// 加载Assetbundle文件(同步)
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="name"></param>
     /// <returns></returns>
-    public AssetBundle LoadAssetbundle(string assetbundle)
+    public AssetBundle LoadAssetbundle(string asset)
     {
-       
         AssetBundle ab = null;
-        if (LoadedAssetbundles.ContainsKey(assetbundle))
+        if (LoadedAssetbundles.ContainsKey(asset))
         {
-            ab = LoadedAssetbundles[assetbundle];
+            ab = LoadedAssetbundles[asset];
         }
         else
         {
             //先加载引用项
-            string[] dependencies = AssetbundleManifest.GetAllDependencies(assetbundle);
+            string[] dependencies = AssetbundleManifest.GetAllDependencies(asset);
 
             for (int i = 0; i < dependencies.Length; i++)
             {
                 string dependence = dependencies[i];
-                Debug.Log("dependence:" + dependence[i]);
+                Debug.Log("这里可能有问题 不确定是不是要加路径 dependence:" + dependence[i]);
 
                 if (LoadedAssetbundles.ContainsKey(dependence))
                 {
@@ -127,10 +135,10 @@ public class ResourceManager : MonoBehaviour {
                 }
             }
             //加载实际项
-            ab = AssetBundle.LoadFromFile(GetAssetbundlePath(assetbundle));
+            ab = AssetBundle.LoadFromFile(GetAssetbundlePath(asset));
 
             //添加进已加载列表中
-            LoadedAssetbundles.Add(assetbundle, ab);
+            LoadedAssetbundles.Add(asset, ab);
         }
         return ab;
     }
@@ -156,10 +164,8 @@ public class ResourceManager : MonoBehaviour {
         {
             t = ab.LoadAsset<T>(asset);
         }
-
         return t;
     }
-
 
     /// <summary>
     /// 加载Assetbundle,加载asset并生成一个资源(同步)
@@ -179,6 +185,64 @@ public class ResourceManager : MonoBehaviour {
         
         return t;
     }
+
+    // ***************************************************************************************************************************
+
+    /// <summary>
+    /// 加载Assetbundle文件,加载asset(异步)
+    /// </summary>
+    /// <param name="assetbundle"></param>
+    //public void LoadFromAssetbundleAsyc<T>(string asset, OnLoadAsset<T> callback) where T : UnityEngine.Object
+    //{
+    //    //如果已加载 则无需加载直接触发回调
+    //    if (LoadedAssetbundles.ContainsKey(asset))
+    //    {
+    //        //TODO
+    //    }
+    //    else
+    //    {
+    //        //如果正在加载中 则在回调中添加本次回调
+    //        if (LoadingAssetbundles.ContainsKey(asset))
+    //        {
+    //            //TODO
+    //        }
+    //        //否则 开启协程加载
+    //        else
+    //        {
+    //            StartCoroutine(IE_LoadFromAssetbundleAsyc<T>(asset, callback));
+    //        }
+    //    }
+    //}
+
+    //private IEnumerator IE_LoadFromAssetbundleAsyc<T>(string asset, OnLoadAsset<T> callback) where T : UnityEngine.Object
+    //{
+    //    //在加载ing列表中添加
+    //    LoadingAssetbundles.Add(asset,null);
+
+    //    AssetBundleCreateRequest abcr = AssetBundle.LoadFromFileAsync(asset);
+    //    yield return abcr;
+    //    if (null != abcr && abcr.isDone)
+    //    {
+    //        AssetBundle ab = abcr.assetBundle;
+
+    //        AssetBundleRequest abr = ab.LoadAssetAsync<T>(asset);
+    //        yield return abr;
+
+    //        if (null != abr && abr.isDone)
+    //        {
+    //            if (null != callback)
+    //            {
+    //                T res = abr.asset as T;
+    //                callback(res);
+    //            }
+    //        }
+    //    }
+
+    //    //移除ing列表
+    //    LoadingAssetbundles.Remove(asset);
+    //}
+
+
 
     #endregion
 
